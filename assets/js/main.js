911678576197d48599e9d57b22a31065bf67d284
Tomasz Pinter <tomaszpinter@gmail.com>
2023-04-18 18:53:54 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+AOS.init();
+
+
+let options = {
+    startDelay: 500,
+    strings: ["FRONTEND DEWELOPER", "BACKEND DEWELOPER", "FULLMASTER DEWELOPER"],
+    typeSpeed: 40,
+    loop: true,
+};
+
+let typed = new Typed('.typed', options);
